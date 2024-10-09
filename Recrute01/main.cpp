cf5d9291824e40dfff5bf5965831e7ba5a0bd119
sumid <sumid@DESKTOP-V506TND>
2024-10-09 15:37:03 +0900

    int->stringに変更

--- Recrute01/main.cpp
+++ Recrute01/main.cpp
@@ -8,12 +8,12 @@ using namespace std;
 struct Question
 {
 	string s;	//問題文
-	int a;		//答え
+	string a;		//答え
 };
 
 int main()
 {
-	vector<Question> questions(3);
+	vector<Question> questions(6);
 
 	random_device rd;
 	mt19937 rand(rd());
@@ -22,13 +22,13 @@ int main()
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
 	questions[0].s = to_string(x) + "x" + to_string(y)+"の答えは？";
-	questions[0].a = x * y;
+	questions[0].a = to_string(x * y);
 
 	//割り算
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
 	questions[1].s = to_string(x*y) + "/" + to_string(y) + "の答えは？";
-	questions[1].a = x;
+	questions[1].a = to_string(x);
 
 	//複雑な式
 	x = uniform_int_distribution<>(1,100)(rand);
@@ -36,40 +36,39 @@ int main()
 	int z = uniform_int_distribution<>(1, 10)(rand);
 	int w = uniform_int_distribution<>(1, 10)(rand);
 	questions[2].s = to_string(x) + "-(" + to_string(y*w) + "+" + to_string(z*w) + ")/" + to_string(w) + "の答えは？";
-	questions[2].a = x - (y + z);
+	questions[2].a = to_string(x - (y + z));
 
 	//三角形の面積
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand)*2;
-	questions.push_back(
-		{
-			"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",x
-		});
+	questions[3].a = "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", to_string(x);
 
 	//円錐の面積
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 3;
-	questions.push_back(
-		{
-			"底辺の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある\n" +
-			"この円錐の体積をXπcm^3とする。Xの値を求めよ。",
-			x*x*y/3
-		});
+	questions[4].a = "底辺の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある\n" +
+		"この円錐の体積をXπcm^3とする。Xの値を求めよ。", to_string(x * x * y / 3);
 
-	cout << "[リクルート試験対策クイズ]\n";
+	//球の面積
+	x = uniform_int_distribution<>(1, 3)(rand)*3;
+	questions[5].a = "半径" + to_string(x) + "cmの球がある\n" +
+		"この球の体積をXπcm^3とする。Xの値を求めよ。",
+		to_string(x * x * x * 4 / 3);
+
+	std::cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions)
 	{
-		cout << e.s << "\n";
-		int answer;
+		std::cout << e.s << "\n";
+		string answer;
 		cin >> answer;
 		if (answer == e.a)
 		{
-			cout << "正解!\n";
+			std::cout << "正解!\n";
 		}
 		else
 		{
-			cout << "間違い!正解は" << e.a << "\n";
+			std::cout << "間違い!正解は" << e.a << "\n";
 		}
 	}	//for questions
 	return 0;
