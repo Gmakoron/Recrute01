438077369b9fe3af92f31008c756e31a4ec7cf41
Gmakoron <ronmako309@gmail.com>
2024-10-16 16:20:24 +0900

    意味から熟語を選ぶ問題に変更

--- Recrute01/exam_japanese.cpp
+++ Recrute01/exam_japanese.cpp
@@ -1,5 +1,6 @@
 #include"exam_japanese.h"
 #include"utility.h"
+#include<random>
 using namespace std;
 
 //感じの読み取り問題を作成する
@@ -12,7 +13,7 @@ QuestionList CreateKanjiExam()
 	} data[] = {
 		{"市井","しせい","人が多く集まって暮らすところ、町"},
 		{"捺印","なついん","(署名とともに)印鑑を押すこと"},
-		{"相殺","そうさい","足し匹の結果、差がなくなること"},
+		{"相殺","そうさい","足しひきの結果、差がなくなること"},
 		{"凡例","はんれい","本や図表のはじめに、使い方や約束事を箇条書きにしたもの"},
 		{"約定","やくじょう","約束して決めること、契約"},
 	};
@@ -21,12 +22,43 @@ QuestionList CreateKanjiExam()
 	QuestionList questions;
 	questions.reserve(quizCount);
 	const vector<int> indices = CreateRandomIndices(size(data));
+	random_device rd;
 
-	for (int i = 0; i < quizCount; i++)
+	//問題の種類を選ぶ
+	int type = uniform_int_distribution<>(0, 1)(rd);
+	if (type == 0)
 	{
-		const auto& e = data[indices[i]];
-		questions.push_back({ "[" + string(e.kanji) + "}の読みをひらがなで答えよ",
-			e.reading });
+		//漢字の読みをこたえる問題
+		for (int i = 0; i < quizCount; i++)
+		{
+			const auto& e = data[indices[i]];
+			questions.push_back({ "[" + string(e.kanji) + "}の読みをひらがなで答えよ",
+				e.reading });
+		}
 	}
+	else
+	{
+		//正しい熟語をこたえる問題
+		for (int i = 0; i < quizCount; i++)
+		{
+			//間違った番号をランダムに選ぶ
+			const int correctIndex = indices[i];
+			vector<int> answers = CreateWrongIndices(size(data), correctIndex);
+
+			//ランダムな位置を正しい番号で上書き
+			const int correctNo = uniform_int_distribution<>(1, 3)(rd);
+			answers[correctNo - 1] = correctIndex;
+
+			//問題文を作成
+			string s = "「" + string(data[correctIndex].meaning) + "」を意味する熟語の番号を選べ\n";
+			for (int j = 0; j < 3; j++)
+			{
+				s += "\n" + to_string(j + 1) + ":" + data[answers[j]].kanji;
+			}
+			questions.push_back({ s,to_string(correctNo) });
+		}
+	}//if type
+
+	
 	return questions;
 }
