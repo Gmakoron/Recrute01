3ecde5958bfa7a3d14d8c0f6251cb67d9b7a180b
Gmakoron <ronmako309@gmail.com>
2024-11-06 15:36:34 +0900

    物理の問題の種類を追加

--- Recrute01/exam_science.cpp
+++ Recrute01/exam_science.cpp
@@ -10,13 +10,39 @@ QuestionList CreatePhysicsExam()
 	questions.reserve(10);
 	random_device rd;
 
-	//等速直線運動
-	constexpr int divisors[] = { 1,2,3,4,5,6,10,12,15,20,30,60 };
-	int i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
-	int v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	//時速
-	int t = uniform_int_distribution<>(0, 10)(rd)*divisors[i];	//時間
-	questions.push_back({ "時速" + to_string(v) + "kmで移動する車がある。\nこの車が" +
-							to_string(v * t / 60) + "km移動するのにかかる時間を分単位で求めよ",to_string(t) });	//等速直線運動
+	{//等速直線運動
+		constexpr int divisors[] = { 1,2,3,4,5,6,10,12,15,20,30,60 };
+		int i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
+		int v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	//時速
+		int t = uniform_int_distribution<>(0, 10)(rd) * divisors[i];	//時間
+		questions.push_back({ "時速" + to_string(v) + "kmで移動する車がある。\nこの車が" +
+		to_string(v * t / 60) + "km移動するのにかかる時間を分単位で求めよ",to_string(t) });
+	}
+
+	{//等加速度直線運動
+	//公式 v = v0 + at　を使って解く問題
+		int v0 = uniform_int_distribution<>(1, 10)(rd);	//初速
+		int a = uniform_int_distribution<>(1, 5)(rd);	//加速度
+		int t = uniform_int_distribution<>(1, 20)(rd);	//移動時間
+		questions.push_back({ "秒速" + to_string(v0) + "mで移動していた車が、進行方向に大きさ" + to_string(a) +
+		"m/sで等加速度直線運動をはじめた。\n" + to_string(t) + "秒後の速度をm/s単位でもとめよ",to_string(v0 + a * t) });
+
+		//公式 x = v0t + 1/2at^2 を使って解く問題
+		v0 = uniform_int_distribution<>(5, 20)(rd);	//初速
+		a = uniform_int_distribution<>(1, 5)(rd);	//加速度
+		t = uniform_int_distribution<>(1, 10)(rd);	//移動時間
+		questions.push_back({ "秒速" + to_string(v0) + "mで移動していた車が、進行方向に大きさ" + to_string(a) +
+		"m/sで等加速度直線運動をはじめた。\n加速をはじめてから" + to_string(t) +
+		"秒後までに移動した距離をm単位で求めよ",to_string(v0 * t + a * t * t / 2) });
+
+	//公式 v^2 - v0^2 = 2ax　を使って解く問題
+		a = -uniform_int_distribution<>(1, 5)(rd)*2;	//加速度
+		t = uniform_int_distribution<>(1, 10)(rd);	//移動時間
+		v0 = -a * t;	//初速
+		questions.push_back({ "秒速" + to_string(v0) + "mで移動していた車がブレーキをかけたところ、" +
+			to_string(v0 * t + a * t * t / 2) + "m進んで停止した(v=0)\nブレーキの加速度をm/s^2単位で求めよ",to_string(a) });
+	}
+
 
 	return questions;
 }
